50c74d857a7ec08636464b4467ccab1ee8ca61ab
28 BYTES LATER <automation@28byteslater.com>
2026-07-29 21:32:47 +0000

    Publish reviewed Homebrew packages
    Source: 28-BYTES-LATER/package-manifests@2055783bd72ec7b7da8232b6acddba46097ac532

--- Casks/kura@rc.rb
+++ Casks/kura@rc.rb
@@ -1,11 +1,11 @@
 cask "kura@rc" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.0.0-rc.2"
-  sha256 arm:          "a9ae15392ae709fab8fecbc8c10dcc9eed82d096e20da19a69272f7dd6c747c1",
-         intel:        "a9ae15392ae709fab8fecbc8c10dcc9eed82d096e20da19a69272f7dd6c747c1",
-         arm64_linux:  "cb58e11da46217b0b2bdd68cb4bca0b4c08dc2b2bb1b39517c9a9feddc29da6b",
-         x86_64_linux: "16b3c10c56e6230b6747352f4b4bd2872ff33660e69d7088b8a09e8b54b78683"
+  version "1.0.0-rc.3"
+  sha256 arm:          "279375b5da60461e01544dc86d82b622847520aaa340c62ab68147619352a5e1",
+         intel:        "279375b5da60461e01544dc86d82b622847520aaa340c62ab68147619352a5e1",
+         arm64_linux:  "d5e1b7a7679a13619247573f1007772c1544de621a288a525da3c4d080500563",
+         x86_64_linux: "b29ac33e6ff945cd93a0c99829c36f7df9ace8ec1533ce2d0d8625cd44792d99"
 
   on_macos do
     url "https://github.com/28-BYTES-LATER/kura/releases/download/v#{version}/kura-v#{version}-macos-universal2.zip"
@@ -16,7 +16,7 @@ cask "kura@rc" do
   end
 
   name "Kura"
-  desc "Kura secure store files"
+  desc "Read secrets from the Kura secure store"
   homepage "https://github.com/28-BYTES-LATER/kura"
 
   conflicts_with cask: "kura"
